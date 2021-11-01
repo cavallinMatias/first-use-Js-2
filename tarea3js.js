8f9e291d5e9b01b4c49f103530ef266bc957eb5d
Matias Cavallin <mati.ignacio2000@gmail.com>
2021-11-01 14:41:39 -0300

    Creando un algoritmo utilizando un ciclo

---
+++ tarea3js.js
@@ -0,0 +1,13 @@
+/* CREAR UN ALGORITMO UTILIZANDO UN CICLO */
+
+let i = prompt("Ingrese un numero")
+while (i <= 15){
+    console.log("Contamos... " + i);
+    i++;
+
+}
+console.log("Fin del conteo");
+
+// 1. Establezco "i" ( valor numero) mediante prompt.
+// 2. Utilizando while ("mientras esto se cumpla...""), sumo +1 veces "i" en cada repeticion/conteo.
+// 3. Finalmente se muestra la salida por cada resultado "Contamos...", hasta llegar al fin del ciclo con "Fin del conteo".
